1708ffef7f5ef4c4a5e09a27bc76f9eaca2bf70e
Jairo Chacón <jchacon@sisintegrados.com>
2024-08-28 18:06:36 -0600

    Add: se agregan nuevas clases de cursos de programación Python donde se vio recursividad, poo y creacion de una biblioteca

---
+++ sumatoria_numeros_naturales.py
@@ -0,0 +1,7 @@
+def sumatoria_numeros_naturales(n):
+    return sum(range(1, n + 1))
+
+# Ejemplo de uso
+numero = 2
+resultado = sumatoria_numeros_naturales(numero)
+print(f"La sumatoria de números naturales hasta {numero} es: {resultado}")
