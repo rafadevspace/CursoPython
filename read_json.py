a0af5cb97b41426c4f2d1a604c02905aa5a33a52
Jairo Chacón <jchacon@sisintegrados.com>
2024-09-23 18:47:12 -0600

    Add: Se agrega funcionalidades de utilización de archivos json, transformar un csv a json y elementos esenciales estadisticos con statistics

---
+++ read_json.py
@@ -0,0 +1,9 @@
+import json
+
+with open('products.json', mode='r') as file:
+    products = json.load(file)
+
+#Mostrar el contenido
+for product in products:
+    #print(product)
+    print(f"Product: {product['name']}, Price: {product['price']}")
---
+++ statistics_data.py
@@ -0,0 +1,41 @@
+import statistics
+import csv
+
+#Leer los datos de ventas mensuales desde un archivo CSV
+monthly_sales = {}
+with open('monthly_sales.csv', mode='r') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        month = row['month']
+        sales = int(row['sales'])
+        monthly_sales[month] = sales
+
+sales = list(monthly_sales.values())
+#print(sales)
+
+#Hallar la media
+mean_sales = statistics.mean(sales)
+print(f"La media es: {mean_sales}")
+
+#Hallar la mediana
+median_sales = statistics.mode(sales)
+print(f"La moda es: {median_sales}")
+
+#Hallar la moda
+mode_sales = statistics.median(sales)
+print(f"La mediana es: {mode_sales}")
+
+#Hallar la desviacion estandar
+stedev_sales = statistics.stdev(sales)
+print(f"La desviación estandar es: {stedev_sales}")
+
+#Hallar la varianza
+variance_sales = statistics.variance(sales)
+print(f"La varianza es: {variance_sales}")
+
+#Maximo y Minimo
+max_sales = max(sales)
+min_sales = min(sales)
+
+range_sales = max_sales - min_sales
+print(f'Rango de ventas: {range_sales}')
